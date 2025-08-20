b41cb48ba037c09b300eda5116528309084153cb
vikramthakur1510 <vikramsingh730209@gmail.com>
2025-08-20 16:12:06 +0530

    changes

---
+++ sqpatern.java
@@ -0,0 +1,20 @@
+public class sqpatern {
+    public static void hollowsq(int r,int c){
+        for(int i = 1;i<=r;i++){
+            for(int j =1;j<=c;j++){
+                if(i==1||i==r||j==1||j==c){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        hollowsq(7,8);
+    }
+    }
+    
+
